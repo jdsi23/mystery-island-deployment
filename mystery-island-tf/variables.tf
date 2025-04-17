3aa92decf87aa6fe835ff23912a38e2bd8216b1b
jdsi23 <jameslusk535@gmail.com>
2025-04-17 20:42:08 +0000

    Updated variables and merged subnet configuration

--- mystery-island-tf/variables.tf
+++ mystery-island-tf/variables.tf
@@ -1,12 +1,12 @@
 variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
+  description = "ami-0c55b159cbfafe1f0"
   type        = string
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.medium"
 }
 
 variable "key_name" {
